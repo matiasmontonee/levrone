99173e6f6b4185cfc58dea9431d469ffb45cc291
Matías <137196771+matiasmontonee@users.noreply.github.com>
2024-06-27 12:00:32 -0300

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from 'react';
 import { auth } from '../firebase';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
 export const useAuth = () => {
   const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
+        setIsAuthenticated(true);
       } else {
         setUser(null);
+        setIsAuthenticated(false);
       }
     });
 
     return () => unsubscribe();
   }, []);
 
-  return { user };
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return { user, isAuthenticated, logout };
 };
